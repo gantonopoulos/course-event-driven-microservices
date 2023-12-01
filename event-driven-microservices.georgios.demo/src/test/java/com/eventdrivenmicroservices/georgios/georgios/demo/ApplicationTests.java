fa93ad129a1364a3fc7cdcfbcf85af9c127eee3b
gantonopoulos <georanto@gmail.com>
2023-12-01 16:10:06 +0100

    Adds generated spring boot project
    Adds the spring boot project as generated from https://start.spring.io/

---
+++ event-driven-microservices.georgios.demo/src/test/java/com/eventdrivenmicroservices/georgios/georgios/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventdrivenmicroservices.georgios.georgios.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
