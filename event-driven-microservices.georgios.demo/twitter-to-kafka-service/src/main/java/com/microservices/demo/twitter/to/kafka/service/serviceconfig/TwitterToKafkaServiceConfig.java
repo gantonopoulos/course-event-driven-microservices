82f37d65db548718d066a2110a6a6c92f62b295d
gantonopoulos <georanto@gmail.com>
2023-12-15 10:59:34 +0100

    Adds the twitter to kafka services and its config
    - Creates an application that will be the service that reads data from
    twitter.
    - Registers the required dependencies of the application.
    - Creates an initial unit test class.
    - Creates a .yml configuration file for defining configuration for the
    new service and adds some configuration entries into it.
    - Creates a configuration package with the class that will read from
    the yml file.

---
+++ event-driven-microservices.georgios.demo/twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/serviceconfig/TwitterToKafkaServiceConfig.java
@@ -0,0 +1,17 @@
+package com.microservices.demo.twitter.to.kafka.service.serviceconfig;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data   // This is lombok related. During compile time it will create boilerplate getters/setters for fields of the class.
+        // These will only be present in the byte-code to be accessed by jvm.
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfig
+{
+    private List<String> twitterKeywords;   // The Name has to match that of the configuration entry in the yml file.
+                                            // twitter-keywords -> twitterKeywords. So the convention is that "-k" -> K.
+}
